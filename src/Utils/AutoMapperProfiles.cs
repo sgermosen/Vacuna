a4900f2e887ae5c89d273158a456c1ada47322c7
sGermosenMac <sgermosen@outlook.com>
2021-09-17 11:28:19 -0400

    Update AutoMapperProfiles.cs

--- src/Utils/AutoMapperProfiles.cs
+++ src/Utils/AutoMapperProfiles.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using NetTopologySuite.Geometries;
 using VacunaAPI.DTOs;
 using VacunaAPI.Entities;
-using NetTopologySuite.Geometries; 
 
 namespace VacunaAPI.Utils
 {
@@ -10,14 +10,36 @@ namespace VacunaAPI.Utils
     {
         public AutoMapperProfiles(GeometryFactory geometryFactory)
         {
-               CreateMap<Inmunization, InmunizationDTO>().ReverseMap();
+            CreateMap<Inmunization, InmunizationDTO>().ReverseMap();
             CreateMap<InmunizationCreationDTO, Inmunization>()
                 .ForMember(x => x.CardPicture,
                                 options => options.Ignore());//  we ignore one or various properties than we want to treat as a diferent way  
-          
+
             CreateMap<IdentityUser, UserDTO>();
+
+            //Working with Latitude
+            //CreateMap<DomainObjectCreationDTO, DomainObject>()
+            //          .ForMember(x => x.Location, x => x.MapFrom(dto =>
+            //           geometryFactory.CreatePoint(new Coordinate(dto.Longitude, dto.Latitude))));
+            //CreateMap<DomainObject, DomainObjectDTO>()
+            //    .ForMember(x => x.Latitude, dto => dto.MapFrom(field => field.Location.Y))
+            //    .ForMember(x => x.Longitude, dto => dto.MapFrom(field => field.Location.X))
+            //    ;
+
+            //Map Details from file of another part of class
+            //CreateMap<DomainObjectCreationDTO, DomainObject>()
+            // .ForMember(x => x.Photo,
+            //                 options => options.Ignore())
+            // .ForMember(x => x.DomainObjectDetails1DTO, options => options.MapFrom(MapDomianObjectDetails1))
+            // .ForMember(x => x.DomainObjectDetails2DTO, options => options.MapFrom(MapDomianObjectDetails2))
+
+            //the reserse part of the one before 
+            //CreateMap<DomainObject, DomainObjectDTO>()
+            //            .ForMember(x => x.Details1, options => options.MapFrom(MapDomainObjectDetails1))
+            //            .ForMember(x => x.Details2, options => options.MapFrom(MapDomainObjectDetails2)) 
+
         }
-         
+
 
 
 
